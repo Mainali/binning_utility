f83811e7b3ad1efe8ac3c61a8224147a19a214b8
Mainali <mainalipukar@gmail.com>
2023-03-26 16:56:15 -0500

    add classes for equal width and equal frequency filter

---
+++ app/EqualFrequencyFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+/**
+ * This class implements equal width filter algorithm
+ */
+class EqualFrequencyFilter extends AbstractBaseDiscreteFilter implements FilterInterface
+{
+
+    function __construct($input)
+    {
+        parent::__construct($input);
+    }
+
+    function filter()
+    {
+        // TODO: Implement filter() method.
+    }
+
+}
---
+++ app/EqualWidthFilter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+class EqualWidthFilter extends AbstractBaseDiscreteFilter implements FilterInterface
+{
+
+    function __construct($input)
+    {
+        parent::__construct($input);
+    }
+
+    function filter()
+    {
+        // TODO: Implement filter() method.
+    }
+
+}
